a04e8e549f89dad1199232b87701766f1abe62b5
nonzINC <nonzvia1@gmail.com>
2026-04-23 15:41:03 +0300

    Update vd-uilib.lua

--- vd-uilib.lua
+++ vd-uilib.lua
@@ -3274,7 +3274,7 @@
         function UILib:ShowDemoMenu()
             self:Unload()
 
-            self:SetMenuSize(Vector2.new(720, 780))
+            self:SetMenuSize(Vector2.new(750, 620))
             self:CenterMenu()
             self:SetMenuTitle('UILib v2 — Full Demo')
 
